1a576abadcdbbdb840f470f929ccbc947a52b16d
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-03-30 15:01:43 +0000

    restful_api tasks 8 & 10 done

---
+++ api/v1/views/places.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" places """
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+from models.place import Place
+from models.city import City
+
+
+@app_views.route("/cities/<uuid:city_id>/places",
+                 methods=['GET', 'POST'],
+                 strict_slashes=False)
+def places(city_id):
+    """places route"""
+    city = storage.get(City, f'{city_id}')
+    if request.method == 'GET':
+        places = city.places
+        return jsonify(list(map(lambda x: x.to_dict(), places)))
+    else:  # POST
+        kwargs = request.get_json(force=True, silent=True)
+        if kwargs is None:
+            abort(400, "Not a JSON")
+        if kwargs.get('name') is None:
+            abort(400, "Missing name")
+        place = Place(**kwargs)
+        city.places.append(place)
+        storage.save()
+        return jsonify(place.to_dict()), 201
+
+
+@app_views.route("/places/<uuid:place_id>",
+                 methods=['GET', 'DELETE', 'PUT'],
+                 strict_slashes=False)
+def place(place_id):
+    """place route"""
+    place = storage.get(Place, f'{place_id}')
+    if place is None:
+        abort(404)
+    if request.method == 'GET':
+        return jsonify(place.to_dict())
+    elif request.method == 'DELETE':
+        place.delete()
+        storage.save()
+        return jsonify({})
+    else:  # PUT
+        kwargs = request.get_json(force=True, silent=True)
+        if kwargs is None:
+            abort(400, "Not a JSON")
+        for key in kwargs:
+            if key not in ('id', 'user_id', 'city_id',
+                           'created_at', 'updated_at'):
+                setattr(place, key, kwargs.get(key))
+        storage.save()
+        return jsonify(place.to_dict())
