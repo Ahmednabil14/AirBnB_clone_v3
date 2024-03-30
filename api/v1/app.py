519f8df3ba40a0da521b80ebc709f029c16d9e4e
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-03-30 21:32:48 +0000

    restful_api task12 corr1

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
